41d17e1d06cc82e0003e7338c626387bf47f9ee1
SakthiRanjani <sakthi.ranjani.k@gmail.com>
2025-04-02 11:21:36 +0200

    Update main.tf

--- web/main.tf
+++ web/main.tf
@@ -30,53 +30,53 @@ resource "aws_instance" "webserver2" {
   }
 }
 
-# resource "aws_lb" "myalb" {
-#   name               = "myalb"
-#   internal           = false
-#   load_balancer_type = "application"
-#   security_groups    = [var.sg]
-#   subnets            = [var.sub1, var.sub2]
+resource "aws_lb" "myalb" {
+   name               = "myalb"
+   internal           = false
+   load_balancer_type = "application"
+   security_groups    = [var.sg]
+   subnets            = [var.sub1, var.sub2]
 
-#   tags = {
-#     Name = "Web"
-#   }
-# }
-
-
-# resource "aws_lb_target_group" "mytg" {
-#   name     = "TargetGroup"
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = var.vpc
-
-#   health_check {
-#     path = "/"
-#     port = "traffic-port"
-#   }
-
-# }
-
-# resource "aws_lb_target_group_attachment" "attach1" {
-#   target_group_arn = aws_lb_target_group.mytg.arn
-#   target_id        = aws_instance.webserver1.id
-#   port             = 80
-# }
+   tags = {
+     Name = "Web"
+   }
+ }
 
 
-# resource "aws_lb_target_group_attachment" "attach2" {
-#   target_group_arn = aws_lb_target_group.mytg.arn
-#   target_id        = aws_instance.webserver2.id
-#   port             = 80
-# }
+ resource "aws_lb_target_group" "mytg" {
+   name     = "TargetGroup"
+   port     = 80
+   protocol = "HTTP"
+   vpc_id   = var.vpc
 
-# resource "aws_lb_listener" "listener" {
-#   load_balancer_arn = aws_lb.myalb.arn
-#   port              = 80
-#   protocol          = "HTTP"
+   health_check {
+     path = "/"
+     port = "traffic-port"
+   }
 
-#   default_action {
-#     target_group_arn = aws_lb_target_group.mytg.arn
-#     type             = "forward"
-#   }
-# }
+ }
+
+resource "aws_lb_target_group_attachment" "attach1" {
+   target_group_arn = aws_lb_target_group.mytg.arn
+   target_id        = aws_instance.webserver1.id
+   port             = 80
+ }
+
+
+ resource "aws_lb_target_group_attachment" "attach2" {
+   target_group_arn = aws_lb_target_group.mytg.arn
+   target_id        = aws_instance.webserver2.id
+   port             = 80
+ }
+
+ resource "aws_lb_listener" "listener" {
+   load_balancer_arn = aws_lb.myalb.arn
+   port              = 80
+   protocol          = "HTTP"
+
+   default_action {
+     target_group_arn = aws_lb_target_group.mytg.arn
+     type             = "forward"
+   }
+ }
 
